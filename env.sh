105fbd0b5f3245c37b05f4cfc8e5c1786d5be9e7
c-guntur <chandra.guntur@gmail.com>
2020-01-25 21:24:04 -0500

    Updated versions of JDKs

--- env.sh
+++ env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export ORACLE_JDK_11_HOME=/Library/Java/JavaVirtualMachines/oraclejdk-11.0.5.jdk/Contents/Home/
-export GRAALVM_EE_19_HOME=/Library/Java/JavaVirtualMachines/graalvm-ee-java11-19.3.0.2/Contents/Home/
-export GRAALVM_CE_19_HOME=/Library/Java/JavaVirtualMachines/graalvm-ce-java11-19.3.0.2/Contents/Home/
+export ORACLE_JDK_11_HOME=/Library/Java/JavaVirtualMachines/oraclejdk-11.0.6.jdk/Contents/Home/
+export GRAALVM_EE_19_HOME=/Library/Java/JavaVirtualMachines/graalvm-ee-java11-19.3.1/Contents/Home/
+export GRAALVM_CE_19_HOME=/Library/Java/JavaVirtualMachines/graalvm-ce-java11-19.3.1/Contents/Home/
 export ADOPT_HOTSPOT_11_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home/
 export ADOPT_OPENJ9_11_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-11-openj9.jdk/Contents/Home/
 export OPENJDK_11_HOME=/Library/Java/JavaVirtualMachines/openjdk-11.0.2.jdk/Contents/Home/
